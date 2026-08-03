b97bdd47cec3759301ad538641872710ad19121c
Marcos Chen <marcoschen@microsoft.com>
2026-08-03 17:46:10 -0300

    updates

--- refunds-dashboard/server/src/utils/asyncHandler.js
+++
@@ -1,5 +0,0 @@
-/** Forwards rejected promises from route handlers to the Express error handler. */
-export function asyncHandler(handler) {
-  return (req, res, next) =>
-    Promise.resolve(handler(req, res, next)).catch(next);
-}
--- refunds-dashboard/server/src/utils/money.js
+++
@@ -1,4 +0,0 @@
-/** Rounds to cents so repeated float math never leaks fractions of a cent. */
-export function roundToCents(amount) {
-  return Math.round(amount * 100) / 100;
-}
